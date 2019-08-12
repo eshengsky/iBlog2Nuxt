ff6726801ac951e173d1476be00cdd44ef316413
Sky <eshengsky@163.com>
2019-08-12 10:52:19 +0800

    commit

--- server/models/comment.js
+++ server/models/comment.js
@@ -14,15 +14,17 @@ const CommentSchema = new mongoose.Schema(Object.assign({}, BaseSchema.obj, {
     avatar: { type: String },
 
     // 评论内容
-    content: { type: String },
-
-    // 回复列表
-    comments: [CommentSchema]
+    content: { type: String }
 }), {
         // 设置查询时默认返回虚拟字段
         toJSON: { virtuals: true },
         toObject: { virtuals: true }
     });
+
+CommentSchema.add({
+    // 回复列表
+    comments: [CommentSchema]
+});
 
 // 虚拟字段：评论时间字符串
 CommentSchema.virtual('commentTimeStr').get(function () {
